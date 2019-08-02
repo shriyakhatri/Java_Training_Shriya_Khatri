41daf0b5e6fbb8863ffe1419574f8fef239c87e6
shriyakhatri <52524004+shriyakhatri@users.noreply.github.com>
2019-08-02 19:12:44 +0530

    Add files via upload

---
+++ task_2/src/com/ct/msa/dao/IMobileDAO.java
@@ -0,0 +1,18 @@
+package com.ct.msa.dao;
+
+import java.util.Map;
+
+import com.ct.msa.model.Mobile;
+
+public interface IMobileDAO {
+	public Map<Integer, Mobile> display();
+
+	public void addMobile(Mobile m);
+
+	public void deleteMobile(int mId);
+
+	public void updateMobileDetails(int mId);
+
+	public Mobile searchMobileById(int mId) throws MobileException;
+
+}
